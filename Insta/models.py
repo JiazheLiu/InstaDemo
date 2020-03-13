796b5bec86a498efc6b3349ea200aa1edf0df528
Jiazhe Liu <Jiazheliu107@gmail.com>
2020-03-13 15:17:51 -0500

    auth

--- Insta/models.py
+++ Insta/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from imagekit.models import ProcessedImageField
 from django.urls import reverse
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class Post(models.Model):
     title = models.TextField(blank=True, null=True)
@@ -13,3 +14,12 @@ class Post(models.Model):
     )
     def get_absolute_url(self):
         return reverse("post_detail", args=[str(self.id)])
+
+class InstaUser(AbstractUser):
+    profile_pic = ProcessedImageField(
+       upload_to = 'static/image/profile',
+       format = 'JPEG',
+       options = {'quality':100},
+       blank = True,
+       null = True 
+    )
